5731ecc188332ee885eddc6ad85231b8994f2d12
teopl <20093089@mail.wit.ie>
2022-03-03 16:25:39 +0000

    Initial commit of IntelliJ console template code

---
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -0,0 +1,39 @@
+package controllers
+
+import models.Note
+
+class NoteAPI {
+
+    private var notes = ArrayList<Note>()
+
+    fun add(note: Note): Boolean {
+        return notes.add(note)
+    }
+
+    fun listAllNotes(): String {
+        return if (notes.isEmpty()) {
+            "No notes stored"
+        } else {
+            var listOfNotes = ""
+            for (i in notes.indices) {
+                listOfNotes += "${i}: ${notes[i]} \n"
+            }
+            listOfNotes
+        }
+    }
+
+    fun numberOfNotes(): Int {
+        return notes.size
+    }
+
+    fun findNote(index: Int): Note? {
+        return if (isValidListIndex(index, notes)) {
+            notes[index]
+        } else null
+    }
+
+    //utility method to determine if an index is valid in a list.
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
+}
