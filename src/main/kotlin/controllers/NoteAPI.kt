35bc567f38c2df9d5d4bd69b4762e3a293263276
Teo Planinec <20093089@mail.wit.ie>
2022-03-22 10:20:08 +0000

    Delete a Note

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -116,4 +116,11 @@ class NoteAPI {
         }
         return counter
     }
+
+    fun deleteNote(indexToDelete: Int): Note? {
+        return if (isValidListIndex(indexToDelete, notes)) {
+            notes.removeAt(indexToDelete)
+        }
+        else null
+    }
 }
